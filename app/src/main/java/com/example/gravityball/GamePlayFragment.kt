9fc9f54e7ea68c5888ae3f1c7f7c2cf21f37a4fc
cheesy d <cheesyd64@hotmail.com>
2023-06-06 14:32:24 -0700

    Configured basic touch listener

--- app/src/main/java/com/example/gravityball/GamePlayFragment.kt
+++ app/src/main/java/com/example/gravityball/GamePlayFragment.kt
@@ -2,10 +2,13 @@ package com.example.gravityball
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
@@ -32,47 +35,42 @@ class GamePlayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_game_play, container, false)
+        val screen: ConstraintLayout = view.findViewById<ConstraintLayout>(R.id.display)
+
+        screen.setOnTouchListener(object: View.OnTouchListener{
+            @SuppressLint("ClickableViewAccessibility")
+            override fun onTouch(view: View, move: MotionEvent):Boolean{
+
+                var eventType: Int = move.getActionMasked()
+
+                when(eventType){
+                    MotionEvent.ACTION_DOWN -> {
+                    Log.i("poop", "touck down is working")
+                    }
+                    MotionEvent.ACTION_UP -> {
+                    Log.i("poop", "touck up is working")
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                    Log.i("poop", "touck move is working")
+                    }
+                }
+                return true;
+            }
+        })
 
         // Initialize platform ImageView
         var platform = view.findViewById<ImageView>(R.id.platform)
 
+
+
         animate(platform)
 
-//                //set platform to imageView and initial position
-//                val platform = view.findViewById<ImageView>(R.id.platForm)
-//                platform.y = -40f
-//
-//                //set which drawable the platform is
-//                val random = (0 until platformList.size).random()
-//                platform.setImageResource(platformList[random])
-//
-//                //gets the starting postion of the platform
-//               val start = (platform.height.toFloat() + (view?.height?.toFloat() ?: 0f))
-//
-//
-//                //set the height of platform so they are all the same height
-//                platform.setMaxHeight(20)
-//
-//                //move the platform
-//                platform.animate().translationYBy(-2100f).duration = 3500
-//
-//                //remove platform from view
-//
-//                platform.setTranslationY(0f);
-//
-//                handler.postDelayed(this, 4000)
-//
         return view
     }
 
     private fun animate(platform: ImageView){
         //make an array of directions to use later
         val position = arrayOf<String>("")
-
-        //get view constraints to change position of platforms
-//        val gamePlayScreen: ConstraintLayout = requireView().findViewById(R.id.gamePlayFragment)
-//        val constraintSet = ConstraintSet()
-//        constraintSet.clone(gamePlayScreen)
 
         //set platform to random platform in list
         val random = Random.nextInt(0, platformList.size)
@@ -85,12 +83,6 @@ class GamePlayFragment : Fragment() {
         val start = (platform.height.toFloat() + (view?.height?.toFloat() ?: 0f))
         val end = -2100f
         var speed: Long = 2000
-//        constraintSet.connect(
-//            platform.id,
-//            ConstraintSet.LEFT,
-//            ConstraintSet.PARENT_ID,
-//            ConstraintSet.LEFT,
-//        )
 
         //move the platform
         val animation = platform.animate().translationYBy(end).setDuration(speed)
